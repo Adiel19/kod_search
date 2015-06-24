8b592de49187cd38934671645cafd34c090c33ed
Adiel19 <adielgranados.19@gmail.com>
2015-06-24 07:23:11 -0500

    works on first page

---
+++ source/actions.py
@@ -0,0 +1,44 @@
+def get_links(driver):
+        # find the class elements
+    class_ = driver.find_elements_by_class_name("title")
+    links = {}
+    for link in class_:
+        links[link.text] = link.find_element_by_css_selector("a").get_attribute("href")
+    return links
+
+import os.path
+
+def write_lit(kod):
+    if "DVD" in lit:
+        return
+    filename = "../lit/kod_lit_spanish_trans.txt"
+
+    if os.path.isfile(filename):
+        f = open(filename, "a")
+        f.write(" ")
+        f.close() 
+    else:
+        f = open(filename, "w")
+        f.write()
+        f.close()
+
+import re
+
+def get_info(title, link, lit_not_spanish, driver):
+
+    lits = []
+    
+    driver.get(link)
+    class_ = driver.find_elements_by_css_selector(".offer.available")
+        
+    for element in class_:
+        lit = element.find_element_by_css_selector(".title").find_element_by_css_selector("a").text
+        if "DVD" in lit:
+            continue
+        elif lit in lit_not_spanish:
+            return 0
+        else:
+            lits.append(element.find_element_by_css_selector(".title").find_element_by_css_selector("a").text)
+    id_ = int(''.join(x for x in link if x.isdigit()))
+
+    return {'title': title, 'id': id_, 'url': link, 'lits': lits}
---
+++ source/search.py
@@ -0,0 +1,29 @@
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from actions import *
+
+display = Display(visible = 0, size = (800, 600))
+display.start()
+
+driver = webdriver.Firefox()
+file_ = open("../lit_not_spanish.txt")
+
+lit_not_spanish = file_.read()
+file_.close()
+
+#print(lit_not_spanish)
+
+for i in range(1,2):
+    driver.get("https://www.thetrumpet.com/key_of_david/program_archive?page=%d" %i)
+    print("Searching https://www.thetrumpet.com/key_of_david/program_archive?page=%d" %i)
+    links = get_links(driver)
+    for link in links:
+        print(link)
+        kod = get_info(link, links[link], lit_not_spanish, driver)
+        if kod == 0:
+            continue
+        else:
+            print(kod)
+            print("\n")
+
+driver.quit()
